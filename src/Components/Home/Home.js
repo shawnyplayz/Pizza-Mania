4f83c0148868a1e8a100ae6c78462bff618b2493
Shawn Almeida <55237616+shawnyplayz@users.noreply.github.com>
2022-11-22 18:04:07 +0530

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,54 @@ import React, { Component } from "react";
 import piz from "../../images/piz.png";
 import "./Home.css";
 export class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pizza: 0,
+      dis: 0,
+      total: 0,
+      initial: 0,
+      addOn: false
+    }
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.total = this.total.bind(this);
+  }
+  async increment() {
+    debugger
+    await this.setState({
+      pizza: this.state.pizza + 1,
+      dis: this.state.dis < 60 ? this.state.dis + 10 : this.state.dis
+    })
+    this.total()
+  }
+  async decrement() {
+    debugger
+    await this.setState({
+      pizza: this.state.pizza > 0 ? this.state.pizza - 1 : this.state.pizza,
+      dis: this.state.dis > 0 ? this.state.dis - 10 : this.state.dis
+    })
+    this.total()
+  }
+  checked() {
+    debugger
+    this.setState({
+      addOn: true,
+      total: this.state.total + 5
+    })
+  }
+  total() {
+    debugger
+    console.log('this.state.pizza', this.state.pizza)
+    let a = (this.state.pizza * 50)
+    let b = (a * this.state.dis / 100);
+    this.setState({
+      initial: this.state.pizza * 50,
+      total: b
+    })
+  }
+
   render() {
     return (
       <div>
@@ -26,15 +74,24 @@ export class Home extends Component {
                               <div className="wrap-content">
                                 <h4 className="my-2">Add Quantity</h4>
                                 <div className="wrap-btns my-2">
-                                  <button className="mx-2">-</button>
-                                  <p className="fw-bold">0</p>
-                                  <button className="mx-2">+</button>
+                                  <button className="mx-2" onClick={this.decrement}>-</button>
+                                  <p className="fw-bold">{this.state.pizza}</p>
+                                  <button className="mx-2" onClick={this.increment}>+</button>
                                 </div>
                                 <div className="form-check my-pad my-2">
                                   <input
                                     className="form-check-input"
                                     type="checkbox"
-                                    value=""
+                                    disabled={this.state.pizza === 0 ? true : false}
+                                    value={this.state.addOn}
+                                    onClick={() =>
+                                      // this.checked
+                                      this.setState(prevState => ({
+                                        addOn: !prevState.addOn
+                                      }))
+                                    }
+                                    checked={this.state.addOn}
+
                                     id="flexCheckDefault"
                                   />
                                   <label
@@ -61,19 +118,20 @@ export class Home extends Component {
                     </div>
                     <div className="secondBox">
                       <div className="wrap-total">
-                        <p className="fw-bold">Total: $150.0</p>
+                        <p className="fw-bold">Total: ${this.state.initial}</p>
                         <div className="wrap-discount">
-                          <p className="fw-bold">Discount</p>
-                          <div className="form-check mx-2">
+                          <p className="fw-bold">Discount : {this.state.dis}</p>
+                          {/* <div className="form-check mx-2">
                             <input
                               className="form-check-input"
                               type="checkbox"
-                              value=""
+                              value={this.state.dis}
                               id="flexCheckDefault"
                             />
-                          </div>
+                          </div> */}
+
                         </div>
-                        <p className="fw-bold">To Pay $135.0</p>
+                        <p className="fw-bold">To Pay ${this.state.total}</p>
                       </div>
                     </div>
                   </div>
@@ -82,7 +140,7 @@ export class Home extends Component {
             </div>
           </div>
         </div>
-      </div>
+      </div >
     );
   }
 }
